72e82f6a805448b4a16389766a1111b786704a3b
Billmetal <billmetal9@gmail.com>
2022-03-08 09:45:47 -0300

    Finalizado o sistema para jogo-da-velha

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # jogo-da-velha
-Criando o famoso jogo da velha com HTML e Javascript
+Criando o famoso jogo da velha com HTML , CSS e Javascript
--- velha.js
+++ velha.js
@@ -9,6 +9,9 @@ var quadrados = document.getElementsByClassName("quadrado");
 mudarJogador("X");
 
 function escolherQuadrado(id){
+    if(vencedor !== null){
+        return;
+    }
     var quadrado = document.getElementById(id);
     if(quadrado.innerHTML !== "-"){
         return;
@@ -49,9 +52,31 @@ function checaVencedor(){
         mudarVencedor(quadrados[6]);
         return;
     }
+    //-----------------------------------------------------------------
     if(checaSequencia(quadrados[0],quadrados[3],quadrados[6])){
         mudaCorQuadrado(quadrados[0],quadrados[3],quadrados[6]);
         mudarVencedor(quadrados[0]);
+        return;
+    }
+    if(checaSequencia(quadrados[1],quadrados[4],quadrados[7])){
+        mudaCorQuadrado(quadrados[1],quadrados[4],quadrados[7]);
+        mudarVencedor(quadrados[1]);
+        return;
+    }
+    if(checaSequencia(quadrados[2],quadrados[5],quadrados[8])){
+        mudaCorQuadrado(quadrados[2],quadrados[5],quadrados[8]);
+        mudarVencedor(quadrados[2]);
+        return;
+    }
+    //-----------------------------------------------------------------
+    if(checaSequencia(quadrados[0],quadrados[4],quadrados[8])){
+        mudaCorQuadrado(quadrados[0],quadrados[4],quadrados[8]);
+        mudarVencedor(quadrados[0]);
+        return;
+    }
+    if(checaSequencia(quadrados[2],quadrados[4],quadrados[6])){
+        mudaCorQuadrado(quadrados[2],quadrados[4],quadrados[6]);
+        mudarVencedor(quadrados[2]);
         return;
     }
 }
@@ -64,4 +89,15 @@ function mudaCorQuadrado(quadrado1,quadrado2,quadrado3){
     quadrado1.style.background = "#0f0";
     quadrado2.style.background = "#0f0";
     quadrado3.style.background = "#0f0";
+}
+
+function reiniciar(){
+    vencedor = null;
+    vencedorSelecionado.innerHTML = "";
+    for(var i = 0; i < quadrados.length; i++){
+        quadrados[i].style.background = "#eee";
+        quadrados[i].style.color = "#eee";
+        quadrados[i].innerHTML = "-";
+    }
+    mudarJogador("X");
 }
