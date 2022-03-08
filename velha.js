930c02999da9148d4225e7c1cd1d11e88599dfdc
Billmetal <billmetal9@gmail.com>
2022-03-07 22:53:20 -0300

    Criado sistema de jogo da velha até parte 7

---
+++ velha.js
@@ -0,0 +1,67 @@
+
+var jogador, vencedor = null;
+
+var jogadorSelecionado = document.getElementById("jogador-selecionado");
+var vencedorSelecionado = document.getElementById("vencedor-selecionado");
+
+var quadrados = document.getElementsByClassName("quadrado");
+
+mudarJogador("X");
+
+function escolherQuadrado(id){
+    var quadrado = document.getElementById(id);
+    if(quadrado.innerHTML !== "-"){
+        return;
+    }
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = "#000";
+    if(jogador === "X"){
+        mudarJogador("O");
+    } else {
+        mudarJogador("X");
+    }
+    checaVencedor();
+}
+
+function mudarJogador(valor){
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
+
+function mudarVencedor(quadrado){
+    vencedor = quadrado.innerHTML;
+    vencedorSelecionado.innerHTML = vencedor;
+}
+
+function checaVencedor(){
+    if(checaSequencia(quadrados[0],quadrados[1],quadrados[2])){
+        mudaCorQuadrado(quadrados[0],quadrados[1],quadrados[2]);
+        mudarVencedor(quadrados[0]);
+        return;
+    }
+    if(checaSequencia(quadrados[3],quadrados[4],quadrados[5])){
+        mudaCorQuadrado(quadrados[3],quadrados[4],quadrados[5]);
+        mudarVencedor(quadrados[3]);
+        return;
+    }
+    if(checaSequencia(quadrados[6],quadrados[7],quadrados[8])){
+        mudaCorQuadrado(quadrados[6],quadrados[7],quadrados[8]);
+        mudarVencedor(quadrados[6]);
+        return;
+    }
+    if(checaSequencia(quadrados[0],quadrados[3],quadrados[6])){
+        mudaCorQuadrado(quadrados[0],quadrados[3],quadrados[6]);
+        mudarVencedor(quadrados[0]);
+        return;
+    }
+}
+
+function checaSequencia(quadrado1,quadrado2,quadrado3){
+    return quadrado1.innerHTML !== "-" && quadrado1.innerHTML === quadrado2.innerHTML && quadrado2.innerHTML === quadrado3.innerHTML;
+}
+
+function mudaCorQuadrado(quadrado1,quadrado2,quadrado3){
+    quadrado1.style.background = "#0f0";
+    quadrado2.style.background = "#0f0";
+    quadrado3.style.background = "#0f0";
+}
